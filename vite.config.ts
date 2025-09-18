8207b6105046325fa65dd5ec9e268029201d060d
Lazzat Meirambekova <lazzat.meirambekova@gmail.com>
2025-09-18 11:08:42 +0500

    избавиться от CORS во время локальной разработки

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+server: {
+  proxy: {
+    '/api': {
+      target: 'http://45.8.116.32',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '/dtj/nsi/api'),
     },
   },
+},
+
 })
