c347f60807035e124bdeab60c7f2eb837ee40497
Meirambekova Lazzat <53467616+ALB040570@users.noreply.github.com>
2025-09-29 16:34:05 +0500

    chore: remove legacy utils http client (#21)

--- src/utils/http.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/http.ts
-import axios from 'axios'
-
-const http = axios.create({
-  baseURL: '/', // важное: не внешний домен!
-  withCredentials: true, // чтобы сессия/куки работали через наш прокси
-})
-
-export default http
