d992beefda2e46cd91886dfae6024775a822e26d
Meirambekova Lazzat <53467616+ALB040570@users.noreply.github.com>
2025-10-01 13:47:35 +0500

    chore: expose object defect crud api (#29)

--- src/features/object-defect-crud/index.ts
+++ src/features/object-defect-crud/index.ts
@@ -1,3 +1,15 @@
-export * from './model/useObjectDefectsQuery'
-export * from './model/useObjectDefectMutations'
-export * from './model/componentSync'
+/** Публичный API фичи CRUD дефектов объектов */
+export { useObjectDefectsQuery } from './model/useObjectDefectsQuery'
+export {
+  useObjectDefectMutations,
+  type RemoveObjectDefectPayload,
+} from './model/useObjectDefectMutations'
+export {
+  createDefectComponentLookup,
+  createDefectCategoryLookup,
+  resolveRemovedDefectValueIds,
+  type DefectComponentLookup,
+  type DefectCategoryLookup,
+  type DefectSelection,
+  type DefectRemoveDiff,
+} from './model/componentSync'
